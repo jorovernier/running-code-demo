687c53bbe2237f1f1d5aa1743ed841dd33f619a7
jorovernier <jorovernier@gmail.com>
2022-12-13 13:10:25 -0700

    finished demo

---
+++ index.js
@@ -0,0 +1,49 @@
+// Congrats on your first Hello World!
+console.log('Hello, World!')
+
+// Variables and Data Types
+
+const firstName = 'Joely'
+
+let age = 22
+
+console.log(firstName, age)
+
+const lovesCats = true
+
+let height;
+
+height = '5ft2in'
+
+console.log(height)
+
+// Math Operators
+
+let x = 4;
+let y = 10;
+
+let z = 7 + 8;
+
+let sum = z + x + y;
+
+console.log(sum)
+
+// Template Strings and Concatenation
+
+let greeting = `Hello, my name is ${firstName} and I am ${age} years old.`
+let greeting2 = 'Hello, my name is ' + firstName + ' and I am ' + age + ' years old.'
+
+console.log(greeting)
+console.log(greeting2)
+
+// Comparison Operators and If Statements
+
+let score = 102
+
+let scoreTwo = 115
+
+if(score === scoreTwo){
+    console.log('First score is equal to score two.')
+} else if(score !== scoreTwo) {
+    console.log('Score is not equal to score two.')
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Running Code Demo
+
+This is a finished version of the Running Code demo. Use this for all your referencing needs!
